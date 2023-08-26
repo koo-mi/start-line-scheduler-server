e37049b05a0bccf8733584e0016ec4bfdbff537e
Mikael <s.h.koo1213@gmail.com>
2023-08-26 19:41:09 -0400

    add initial version of summary

--- controllers/direction-controller.ts
+++ controllers/direction-controller.ts
@@ -5,17 +5,25 @@ const axios = require("axios");
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
-/* Get all location data */
-async function getDirectionData(req: Request, res: Response) {
+/* Get Direction data using Google Map API */
+export async function getDirectionData(req: Request, res: Response) {
     // Authorization 
     const decode = authorize(req, res);
     if (!decode) { return };
 
     const { origin, dest, time, mode } = req.params;
 
+    const directionRes = await directionApi(origin, dest, time, mode);
+
+    res.status(200).json(directionRes);
+}
+
+
+/* For Google Map Direction API call - for direction & summary */
+export async function directionApi(origin: string, dest: string, time: string, mode: string) {
     // Get data from Google Directions API
-    const URL = "https://maps.googleapis.com/maps/api/directions";
-    const API_KEY = process.env.GOOGLE_API_KEY;
+    const URL: string = process.env.GOOGLE_DIRECTION_URL;
+    const API_KEY: string = process.env.GOOGLE_API_KEY;
 
     const data = await axios(`${URL}/json?origin=${origin}&destination=${dest}&arrival_time=${time}&mode=${mode}&key=${API_KEY}`);
 
@@ -28,8 +36,7 @@ async function getDirectionData(req: Request, res: Response) {
     const duration = directionData.duration.text;
     const stepsFull = directionData.steps;
 
-    const stepsSummary = stepsFull.map((step)=> {
-
+    const stepsSummary = stepsFull.map((step) => {
         return {
             distance: step.distance.text,
             duration: step.duration.text,
@@ -37,14 +44,7 @@ async function getDirectionData(req: Request, res: Response) {
         }
     })
 
-    // Response Data
-    const directionRes = {
+    return {
         arrivalTime, departureTime, distance, duration, stepsSummary
     }
-
-    res.status(200).json(directionRes);
-}
-
-module.exports = {
-    getDirectionData
 }
--- routes/summary.ts
+++ routes/summary.ts
@@ -6,16 +6,43 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 const authorize = require("../utils/authorize");
+import { directionApi } from "../controllers/direction-controller";
 
 router.route("/")
     // Get summarized data for the homepage
-    .get((req: Request, res: Response) => {
+    .post(async (req: Request, res: Response) => {
+        // Authorization
         const decode = authorize(req, res);
+        if (!decode) { return };
+        const { profile_id } = decode;
 
-        // If not authorized
-        if (!decode) {return};
+        // Validate
+        const { origin, dest, time, mode } = req.body;
 
-        return res.send(decode);
+        // Get location Data
+        const locationData = await prisma.location.findMany({
+            where: {
+                user_ProfileId: profile_id
+            }
+        });
+
+        // Get direction Data
+        const directionData = await directionApi(origin, dest, time, mode)
+
+        // Get checklist Data
+        const checklistData = await prisma.checklist.findMany({
+            where: {
+                user_ProfileId: profile_id
+            }
+        })
+
+        const summaryRes = {
+            locationData,
+            directionData,
+            checklistData
+        }
+
+        res.json(summaryRes);
     })
 
 module.exports = router;
